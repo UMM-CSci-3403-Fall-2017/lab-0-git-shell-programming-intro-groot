9627dde05ae7515b46aa9ed0e142a57cc4d6b2d1
mitch723 <mitch723@morris,umn.edu>
2017-08-31 11:41:23 -0500

    test

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -1,25 +1,23 @@
 #!/usr/bin/bash
 
+
 FILE=$1
 tmp_dir=$2
 
 tar -xf $FILE --directory $tmp_dir
 
-here=$(tar -tzf $FILE | head -1 | cut -f1 -d"/")
-echo $here
+target_dir=$(tar -tzf $FILE | head -1 | cut -f1 -d"/")
+parent_dir=$(pwd)
 
-cd $tmp_dir/$here
+cd $tmp_dir
+scratch_dir=$(pwd)
+cd $target_dir
 
-if [ "$here" == "little_dir" ]
-then
-	cleaned="cleaned_"
-	cleaned_filename="$cleaned$here"
-	echo $cleaned_filename
-	echo "got here"
-	pwd
-fi
+rm $(grep -lir "DELETE ME!") 
 
-rm $(grep -lir "DELETE ME!")
-
-echo "Removed Delete me!"
+cleaned="cleaned_"
+cleaned_filename="$cleaned$target_dir"
+cd $scratch_dir
+tar -zcf $parent_dir/$cleaned_filename.tgz $target_dir
+ls -l | wc
 
